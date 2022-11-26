da748322806467d3565af66a62fc986f25133f5f
caaallum <callum_speirs@protonmail.com>
2022-11-26 23:27:32 +0000

    Update components

--- src/main.js
+++ src/main.js
@@ -5,21 +5,34 @@ import VueFullPage from 'vue-fullpage.js'
 const app = createApp(App);
 
 // Primevue components
-import Toolbar from 'primevue/toolbar';
 import Button from 'primevue/button';
 import Timeline from 'primevue/timeline';
 import InputText from 'primevue/inputtext';
 import TextArea from 'primevue/textarea';
 import Toast from 'primevue/toast';
+import Galleria from 'primevue/galleria';
+import Image from 'primevue/image';
+import Dialog from 'primevue/dialog';
+import Tooltip from 'primevue/tooltip';
+import Chip from 'primevue/chip';
+
 import ToastService from 'primevue/toastservice';
+import DialogService from 'primevue/dialogservice';
+
+app.directive('tooltip', Tooltip);
 
 app.use(ToastService);
-app.component('Toolbar', Toolbar);
+app.use(DialogService);
+
 app.component('Button', Button);
 app.component('Timeline', Timeline);
 app.component('InputText', InputText);
 app.component('TextArea', TextArea);
 app.component('Toast', Toast);
+app.component('Galleria', Galleria);
+app.component('Image', Image);
+app.component('Dialog', Dialog);
+app.component('Chip', Chip);
 
 // Theme
 import 'primevue/resources/primevue.min.css';
@@ -27,6 +40,7 @@ import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 import 'primevue/resources/themes/bootstrap4-dark-blue/theme.css';
 import 'vue-fullpage.js/dist/style.css'
+import 'devicon/devicon.min.css';
 
 app.use(PrimeVue, { ripple: true });
 app.use(VueFullPage)
